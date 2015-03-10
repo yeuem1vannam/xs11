cd57b00c65fdce71b848957577ab51232db91b07
Phuong 'J' Le H <le.hoai.phuong@framgia.com>
2015-03-10 13:59:51 +0700

    Sync data from SQLite3 to MySQL

--- db/schema.rb
+++ db/schema.rb
@@ -14,29 +14,28 @@
 ActiveRecord::Schema.define(version: 20150309032824) do
 
   create_table "players", force: :cascade do |t|
-    t.integer  "team_id",    limit: 4
-    t.string   "name",       limit: 255
-    t.integer  "uid",        limit: 4
-    t.integer  "team_uid",   limit: 4
-    t.integer  "league_uid", limit: 4
-    t.integer  "grade",      limit: 4
-    t.text     "info",       limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.integer  "team_id"
+    t.string   "name"
+    t.integer  "uid"
+    t.integer  "team_uid"
+    t.integer  "league_uid"
+    t.integer  "grade"
+    t.text     "info"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "players", ["team_id"], name: "index_players_on_team_id", using: :btree
+  add_index "players", ["team_id"], name: "index_players_on_team_id"
 
   create_table "teams", force: :cascade do |t|
-    t.string   "login_name",   limit: 255
-    t.integer  "uid",          limit: 4
-    t.integer  "team_uid",     limit: 4
-    t.integer  "league_uid",   limit: 4
-    t.integer  "member_count", limit: 4
-    t.integer  "league_count", limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.string   "login_name"
+    t.integer  "uid"
+    t.integer  "team_uid"
+    t.integer  "league_uid"
+    t.integer  "member_count"
+    t.integer  "league_count"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
-  add_foreign_key "players", "teams"
 end
